ceddcc6265488be21fcf5022e885c3b94c3a9996
Галина <galocka@MacBook-Galina.local>
2020-02-10 22:22:06 +0200

    template

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+import dishes from "./menu.json";
+import dishItemTemplate from "./templates/dish.hbs"
+
 import './styles.css';
+
+const refs = {
+    dishFeed: document.querySelector('.js-menu'),
+}
+
+function dishCollection (dishes) {
+    const markup = dishes.reduce(((acc, dish) => acc + dishItemTemplate(dish)), "");
+    refs.dishFeed.insertAdjacentHTML('beforeend', markup);
+}
+dishCollection(dishes);
+
